b3370b843a8f914ea9c3b04921b60ec522db1f25
Rosa WANG <rosa@MacBook-Pro.local>
2023-03-27 21:13:29 -0400

    Initial commit

---
+++ lambdas/LF2/search-photos.py
@@ -0,0 +1,97 @@
+import json
+import math
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+import boto3
+import requests
+import urllib.parse
+from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+headers = {"Content-Type": "application/json"}
+host = 'https://search-photo-search-d7qcrc4oxwpstfmjnbvcbhvgcq.us-west-2.es.amazonaws.com/'
+region = 'us-west-2'
+lex = boto3.client('lex-runtime', region_name=region)
+
+
+def lambda_handler(event, context):
+    print('event : ', event)
+
+    q1 = event["queryStringParameters"]['q']
+
+    # if(q1 == "searchAudio" ):
+    #     q1 = convert_speechtotext()
+
+    print("q1:", q1)
+    labels = get_labels(q1)
+    print("labels", labels)
+    if len(labels) != 0:
+        img_paths = get_photo_path(labels)
+
+    if not img_paths:
+        return {
+            'statusCode': 200,
+            "headers": {"Access-Control-Allow-Origin": "*"},
+            'body': json.dumps('No Results found')
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'headers': {"Access-Control-Allow-Origin": "*"},
+            'body': {
+                'imagePaths': img_paths,
+                'userQuery': q1,
+                'labels': labels,
+            },
+            'isBase64Encoded': False
+        }
+
+
+def get_labels(query):
+    response = lex.post_text(
+        botName='bot1',
+        botAlias='$LATEST',
+        userId="I1YDNCYLAC",
+        inputText=query
+    )
+    print("lex-response", response)
+
+    labels = []
+    if 'slots' not in response:
+        print("No photo collection for query {}".format(query))
+    else:
+        print("slot: ", response['slots'])
+        slot_val = response['slots']
+        for key, value in slot_val.items():
+            if value != None:
+                labels.append(value)
+    return labels
+
+
+def get_photo_path(keys):
+    es = Elasticsearch(
+        [host],
+        http_auth=("admin", "WYHram1220$"),
+    )
+
+    resp = []
+    for key in keys:
+        if (key is not None) and key != '':
+            searchData = es.search({"query": {"match": {"labels": key}}})
+            resp.append(searchData)
+    print(resp)
+    output = []
+    for r in resp:
+        if 'hits' in r:
+            for val in r['hits']['hits']:
+                key = val['_source']['objectKey']
+                if key not in output:
+                    output.append('https://s3.us-west-2.amazonaws.com/imgorig/' + key)
+    print(output)
+    return output
